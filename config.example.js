c589cc189402f15cb68388d064886a517c49785e
AnvaUserBot <98270034+shakoHSYNV@users.noreply.github.com>
2022-02-12 02:34:15 +0400

    Update config.example.js

--- config.example.js
+++ config.example.js
@@ -1,5 +1,5 @@
 module.exports = {
-	token: "1234567890:XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+	token: "5143694635:AAEh23qtH-lqEBaLNbCUTBUmjiX5V5-tzF0",
 	botId: 1234567890, //first numbers of BOT_TOKEN
 	rounds: 5, //rounds amount
 	waitDelay: 12000, //1 round duration
