63667310dd1d4ffb18720145519128b66ed90b85
Husuyekdi <96442932+Husuyekdi@users.noreply.github.com>
2022-05-19 16:34:09 +0400

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-	token: "5254702593:AAH36Z6sX4icWeeWbIEerD3IPzwu14wcLQA",
-	botId: 5254702593, //first numbers of BOT_TOKEN
+	token: "5396100854:AAEUGCAjy0H9UgHZMDVNdLY8D2CXlkppSg0",
+	botId: 5396100854, //first numbers of BOT_TOKEN
 	rounds: 5, //rounds amount
 	waitDelay: 12000, //1 round duration
 	timerSteps: 3, //timer indicators
