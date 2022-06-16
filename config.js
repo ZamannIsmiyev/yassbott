3f09d9dcbcd460e26af4ec661c09e2d5755c4aa1
ZamannIsmiyev <99416682+ZamannIsmiyev@users.noreply.github.com>
2022-06-16 04:43:12 +0400

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-	token: "5396100854:AAEUGCAjy0H9UgHZMDVNdLY8D2CXlkppSg0",
+	token: "5583915308:AAGQvHHmKaHzRdqXSNON3EQB6x-07D6CInA",
 	botId: 5396100854, //first numbers of BOT_TOKEN
 	rounds: 5, //rounds amount
 	waitDelay: 12000, //1 round duration
